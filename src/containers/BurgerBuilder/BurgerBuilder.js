385fba698269ada70132bf5e91cabf9c8826feef
chenhe-henry <chenhe.elec@gmail.com>
2020-06-08 20:45:53 +1000

    dynamic burger builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,22 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends React.Component {
-  state = {};
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...}
+  // }
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    },
+  };
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Controls</div>
       </Aux>
     );
