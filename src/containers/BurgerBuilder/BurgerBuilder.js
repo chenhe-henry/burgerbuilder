359bf4e03555e4895ea3541f472c875d220ca626
chenhe-henry <chenhe.elec@gmail.com>
2020-06-09 16:12:30 +1000

    test code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,6 +20,7 @@ class BurgerBuilder extends React.Component {
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <div>Burger Controls</div>
+        <div>Test</div>
       </Aux>
     );
   }
